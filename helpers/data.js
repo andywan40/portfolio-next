0abb73b92235e1826af86873dca89a0cdd1e210f
andywan40 <andywan40@gmail.com>
2021-07-17 09:47:25 +0800

    update [project] page content, styles and images

--- helpers/data.js
+++ helpers/data.js
@@ -4,8 +4,10 @@ import harajuku from "../public/harajuku.jpg";
 import shibuya from "../public/shibuya.jpg";
 import foodpicker from "../public/foodpicker.jpg";
 import foodpicker1 from "../public/foodpicker1.png";
+import foodpicker2 from "../public/foodpicker2.png";
 import wannabuy from "../public/wannabuy.png";
 import wannabuy1 from "../public/wannabuy1.png";
+import wannabuy2 from "../public/wannabuy2.png";
 
 export const projects = [
   {
@@ -17,6 +19,7 @@ export const projects = [
     image: "./foodpicker.jpg",
     img: foodpicker,
     img1: foodpicker1,
+    img2: foodpicker2,
     link: "https://react-foodpicker-app.herokuapp.com",
     year: "2021",
     id: 0,
@@ -37,6 +40,7 @@ export const projects = [
     image: "./wannabuy.png",
     img: wannabuy,
     img1: wannabuy1,
+    img2: wannabuy2,
     link: "https://powerful-journey-37111.herokuapp.com",
     year: "2020",
     id: 1,
--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -1,30 +1,20 @@
-import { useRouter } from "next/router";
 import Image from "next/image";
 import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 import Page from "../../components/Page";
 import { projects } from "../../helpers/data";
 
 export default function Project({ project }) {
-  //const router = useRouter();
-  //const projectTitle = router.query.project;
-  //let projectObj = {};
-  // for (let project of projects) {
-  //   if (project.title.toLowerCase() === projectTitle) {
-  //     projectObj = project;
-  //   }
-  // }
-
   return (
     <Page>
-      <div className="container mt-5 xxs:px-1 xs:px-4 sm:px-8 lg:px-20 px-30 py-10 flex flex-col">
+      <div className="container mt-5 xxs:px-1 xs:px-4 sm:px-8 md:px-15 lg:px-20 px-30 py-10 flex flex-col">
         <div className="grid grid-cols-12 mb-20">
-          <div className="flex flex-col col-start-1 col-span-4 justify-center items-center p-5">
+          <div className="flex flex-col col-start-1 col-span-4 lg:col-start-2 lg:col-span-10 lg:mb-10 justify-center items-center p-5 lg:pt-2">
             <h1 className="text-black text-3xl font-title">{project.title}</h1>
             <h6 className="text-gray-700 text-sm font-title">
               {project.subtitle}
             </h6>
           </div>
-          <div className="w-full h-full col-start-5 col-span-6">
+          <div className="w-full h-full col-start-5 col-span-6 lg:col-start-2 lg:col-span-10">
             <Image
               src={project.img}
               alt={project.title}
@@ -33,19 +23,26 @@ export default function Project({ project }) {
           </div>
         </div>
         <div className="grid grid-cols-12 mb-20">
-          <div className="w-full h-full col-start-2 col-span-6">
+          <div className="lg:hidden w-full h-full col-start-2 col-span-6 lg:col-start-2 lg:col-span-6 flex flex-col justify-center items-center p-5">
+            <div className="mb-16">
+              <Image
+                src={project.img1}
+                alt={project.title}
+                className="w-full h-full rounded-lg"
+              />
+            </div>
             <Image
-              src={project.img1}
+              src={project.img2}
               alt={project.title}
-              className="w-full h-full rounded-lg "
+              className="w-full h-full rounded-lg"
             />
           </div>
-          <div className="flex flex-col col-start-8 col-span-5 justify-start items-start text-left pt-5 p-20">
-            <div className="my-5">
+          <div className="flex flex-col lg:col-start-2 lg:col-span-11 col-start-8 col-span-5 justify-start items-start text-left pt-5 px-10 md:px-5 lg:px-8 ">
+            <div className="my-5 lg:p-2">
               <h2 className="font-bold uppercase text-black mb-1">Year</h2>
               <h2>{project.year}</h2>
             </div>
-            <div className="my-5">
+            <div className="my-5 lg:p-2">
               <h2 className="font-bold uppercase text-black mb-1">Stack</h2>
               <ul className="ml-3">
                 {project?.technology.map(tech => (
@@ -55,19 +52,35 @@ export default function Project({ project }) {
                 ))}
               </ul>
             </div>
-            <div className="my-5 w-1/2">
+            <div className="my-5 lg:p-2 w-1/2 lg:w-full">
               <h2 className="font-bold uppercase text-black mb-1">Mission</h2>
               <h2>{project.description}</h2>
             </div>
-            <a
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-content uppercase text-white bg-black py-3 px-5 mt-5 rounded-sm"
-            >
-              Visit Site
-              <ArrowNarrowRightIcon className="w-5 h-5 ml-5 inline-block"></ArrowNarrowRightIcon>
-            </a>
+            <div className="lg:flex lg:flex-col lg:justify-center lg:h-full">
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-content uppercase text-white bg-black py-3 px-5 mt-5 rounded-sm lg:py-1 lg:px-3 flex items-center sm:text-sm"
+              >
+                Visit Site
+                <ArrowNarrowRightIcon className="w-5 h-5 ml-5 inline-block"></ArrowNarrowRightIcon>
+              </a>
+            </div>
+          </div>
+          <div className="hidden lg:block w-full h-full lg:col-start-2 lg:col-span-11 flex flex-col justify-center items-center p-5 lg:mt-10">
+            <div className="mb-16">
+              <Image
+                src={project.img1}
+                alt={project.title}
+                className="w-full h-full rounded-lg"
+              />
+            </div>
+            <Image
+              src={project.img2}
+              alt={project.title}
+              className="w-full h-full rounded-lg"
+            />
           </div>
         </div>
       </div>
