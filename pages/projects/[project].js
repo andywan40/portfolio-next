8b5059854463854cb493f54bbaac0e4a6e32255e
andywan40 <andywan40@gmail.com>
2021-07-17 11:45:47 +0800

    update [project] page content

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -9,15 +9,15 @@ export default function Project({ project }) {
       <div className="container mt-5 xxs:px-1 xs:px-4 sm:px-8 md:px-15 lg:px-20 px-30 py-10 flex flex-col">
         <div className="grid grid-cols-12 mb-20">
           <div className="flex flex-col col-start-1 col-span-4 lg:col-start-2 lg:col-span-10 lg:mb-10 justify-center items-center p-5 lg:pt-2">
-            <h1 className="text-black text-3xl font-title">{project.title}</h1>
+            <h1 className="text-black text-3xl font-title">{project?.title}</h1>
             <h6 className="text-gray-700 text-sm font-title">
-              {project.subtitle}
+              {project?.subtitle}
             </h6>
           </div>
           <div className="w-full h-full col-start-5 col-span-6 lg:col-start-2 lg:col-span-10">
             <Image
-              src={project.img}
-              alt={project.title}
+              src={project?.img}
+              alt={project?.title}
               className="w-full h-full rounded-lg"
             />
           </div>
@@ -26,21 +26,21 @@ export default function Project({ project }) {
           <div className="lg:hidden w-full h-full col-start-2 col-span-6 lg:col-start-2 lg:col-span-6 flex flex-col justify-center items-center p-5">
             <div className="mb-16">
               <Image
-                src={project.img1}
-                alt={project.title}
+                src={project?.img1}
+                alt={project?.title}
                 className="w-full h-full rounded-lg"
               />
             </div>
             <Image
-              src={project.img2}
-              alt={project.title}
+              src={project?.img2}
+              alt={project?.title}
               className="w-full h-full rounded-lg"
             />
           </div>
           <div className="flex flex-col lg:col-start-2 lg:col-span-11 col-start-8 col-span-5 justify-start items-start text-left pt-5 px-10 md:px-5 lg:px-8 ">
             <div className="my-5 lg:p-2">
               <h2 className="font-bold uppercase text-black mb-1">Year</h2>
-              <h2>{project.year}</h2>
+              <h2>{project?.year}</h2>
             </div>
             <div className="my-5 lg:p-2">
               <h2 className="font-bold uppercase text-black mb-1">Stack</h2>
@@ -54,11 +54,11 @@ export default function Project({ project }) {
             </div>
             <div className="my-5 lg:p-2 w-1/2 lg:w-full">
               <h2 className="font-bold uppercase text-black mb-1">Mission</h2>
-              <h2>{project.description}</h2>
+              <h2>{project?.description}</h2>
             </div>
             <div className="lg:flex lg:flex-col lg:justify-center lg:h-full">
               <a
-                href={project.link}
+                href={project?.link}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="font-content uppercase text-white bg-black py-3 px-5 mt-5 rounded-sm lg:py-1 lg:px-3 flex items-center sm:text-sm"
@@ -71,14 +71,14 @@ export default function Project({ project }) {
           <div className="hidden lg:block w-full h-full lg:col-start-2 lg:col-span-11 flex flex-col justify-center items-center p-5 lg:mt-10">
             <div className="mb-16">
               <Image
-                src={project.img1}
-                alt={project.title}
+                src={project?.img1}
+                alt={project?.title}
                 className="w-full h-full rounded-lg"
               />
             </div>
             <Image
-              src={project.img2}
-              alt={project.title}
+              src={project?.img2}
+              alt={project?.title}
               className="w-full h-full rounded-lg"
             />
           </div>
